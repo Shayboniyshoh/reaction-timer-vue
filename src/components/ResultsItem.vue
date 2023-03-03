b76ea5cdac62400795b750f326f7712dd5d6cf83
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2023-03-03 21:10:37 +0500

    added a rank for the score range

--- src/components/ResultsItem.vue
+++ src/components/ResultsItem.vue
@@ -1,10 +1,28 @@
 <template>
     <p>Your reaction time: {{ this.score }} ms</p>
+    <p>{{ this.rank }}</p>
 </template>
 
 <script>
 export default {
     props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+
+    mounted() {
+        if (this.score < 150) {
+            this.rank = "You are a SUPER-DUPER!!!";
+        } else if (this.score < 250) {
+            this.rank = "You have an excellent Reaction!!!";
+        } else if (this.score > 250 && this.score < 500) {
+            this.rank = "You are like quick snail...:D";
+        } else {
+            this.rank = "Ohhh, You are Turbo...:DDD";
+        }
+    }
 }
 </script>
 
