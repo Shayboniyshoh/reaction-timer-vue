a2c1bb42bfc5a557667e0e7a0906162f30ba381e
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2023-03-03 18:56:42 +0500

    Start game settings configured

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <h1>Reaction Timer App</h1>
+  <button @click="startGame" :disabled="isPlaying">Start</button>
+  <BlockItem v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
 
+import BlockItem from "./components/BlockItem.vue";
+
 export default {
   name: 'App',
-  components: {
+  components: { BlockItem },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    startGame() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background: #555;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +38,9 @@ export default {
   text-align: center;
   color: #333;
   margin-top: 60px;
+}
+
+h1 {
+  color: #fff;
 }
 </style>
---
+++ src/components/BlockItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="block">
+        <h1>Click me</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    padding: 100px 0;
+    margin: 40px auto;
+    border-radius: 1rem;
+    text-align: center;
+    color: #fff;
+    background: #222;
+}
+</style>
