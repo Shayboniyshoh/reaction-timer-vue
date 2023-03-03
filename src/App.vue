4f28a14af907f37f49d4184afa718adcf6d7e184
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2023-03-03 20:50:00 +0500

    showing results and restarting the game

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Reaction Timer App</h1>
   <button @click="startGame" :disabled="isPlaying">Start</button>
-  <BlockItem v-if="isPlaying" :delay="delay" />
+  <BlockItem v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResult">Your reaction time: {{ score }} ms</p>
 </template>
 
 <script>
@@ -14,13 +15,21 @@ export default {
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResult: false
     }
   },
   methods: {
     startGame() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.isPlaying = false;
+      this.score = reactionTime;
+      this.showResult = true;
     }
   }
 }
@@ -40,7 +49,8 @@ body {
   margin-top: 60px;
 }
 
-h1 {
+h1,
+p {
   color: #fff;
 }
 </style>
